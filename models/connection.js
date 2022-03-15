6b2991869ae7dffd19ea80aac4d788c86a29c428
fql <77008470+fengql123@users.noreply.github.com>
2022-03-15 12:23:51 +0800

    deploy

--- models/connection.js
+++ models/connection.js
@@ -3,13 +3,8 @@ const conString = process.env.CONNECTION;
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(conString, {
+const sequelize = new Sequelize(precess.env.DATABASE_URL, {
   dialect: "postgres",
-  logging,
-  ssl: false,
-  dialectOptions: {
-    ssl: { rejectUnauthorized: false },
-  },
 });
 
 console.log(DB_PASSWORD);
