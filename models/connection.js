cf6b2cb7733faa160b5a2edd2283158185301e59
fql <77008470+fengql123@users.noreply.github.com>
2022-03-15 12:12:41 +0800

    deploy

--- models/connection.js
+++ models/connection.js
@@ -3,9 +3,13 @@ const conString = process.env.CONNECTION;
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("ecommerce", DB_USERNAME, DB_PASSWORD, {
-  host: "localhost",
+const sequelize = new Sequelize(conString, {
   dialect: "postgres",
+  logging,
+  ssl: false,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
 });
 
 console.log(DB_PASSWORD);
