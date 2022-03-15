a05d18897335172c7a36f1ad79dc6cb3f320581b
fql <77008470+fengql123@users.noreply.github.com>
2022-03-15 12:06:22 +0800

    deploy

--- models/connection.js
+++ models/connection.js
@@ -3,6 +3,10 @@ const conString = process.env.CONNECTION;
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(conString);
+const sequelize = new Sequelize("ecommerce", DB_USERNAME, DB_PASSWORD, {
+  host: "heroku",
+  dialect: "postgres",
+});
 
+console.log(DB_PASSWORD);
 module.exports = sequelize;
